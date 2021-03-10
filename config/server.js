34be05a004d489566edf3df38a92a5bc422de000
Brian Ketelsen <mail@bjk.fyi>
2021-03-10 12:09:07 -0500

    first post

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7153f775f5866e022f699fe050c2d7cd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '052ab3bd-131c-4eb2-89bb-8f1f39c05387'
+};
